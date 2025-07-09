aaaae1d0d7d4642f03dcca055aee27163d152d6d
Денис Алексеев <fanaticFiz@outlook.com>
2025-07-09 10:01:03 +0300

    oauth2-crg-plugin implementation

---
+++ src/community/security/oauth2-crg/src/main/java/org/geoserver/security/oauth2/services/GoogleTokenServices.java
@@ -0,0 +1,55 @@
+/* (c) 2016 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.security.oauth2.services;
+
+import io.jsonwebtoken.Jwts;
+import org.geoserver.security.oauth2.GeoServerOAuthRemoteTokenServices;
+
+import java.util.Base64;
+import java.util.Map;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+/**
+ * Remote Token Services for Google token details.
+ *
+ * @author Alessio Fabiani, GeoSolutions S.A.S.
+ */
+public class GoogleTokenServices extends GeoServerOAuthRemoteTokenServices {
+
+    public GoogleTokenServices() {
+        super(new GoogleAccessTokenConverter());
+    }
+
+    @Override
+    protected Map<String, Object> checkToken(String accessToken) {
+        try {
+            return Jwts.parser()
+                       .setSigningKey(clientSecret.getBytes())
+                       .parseClaimsJws(accessToken)
+                       .getBody();
+        } catch (Exception e) {
+            LOGGER.error("Failed to parse access token. Reason => {}", e.getMessage(), e);
+
+            return Map.of();
+        }
+    }
+
+    @Override
+    protected void transformNonStandardValuesToStandardValues(Map<String, Object> map) {
+        LOGGER.debug("Original map = {}", map);
+
+        map.put("client_id", map.get("user_name"));
+
+        LOGGER.debug("Transformed = {}", map);
+    }
+
+    @Override
+    protected String getAuthorizationHeader(String accessToken) {
+        String creds = String.format("%s:%s", clientId, clientSecret);
+
+        return "Basic " + new String(Base64.getEncoder().encode(creds.getBytes(UTF_8)));
+    }
+}
