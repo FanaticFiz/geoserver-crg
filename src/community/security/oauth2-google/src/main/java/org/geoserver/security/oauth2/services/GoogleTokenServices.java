60a8129a9f81c7c5f1280bfb93fed572ac3c7b5b
Денис Алексеев <fanaticFiz@outlook.com>
2025-10-28 11:58:45 +0300

    oauth2-crg-plugin implementation

--- src/community/security/oauth2-google/src/main/java/org/geoserver/security/oauth2/services/GoogleTokenServices.java
+++ src/community/security/oauth2-google/src/main/java/org/geoserver/security/oauth2/services/GoogleTokenServices.java
@@ -4,13 +4,13 @@
  */
 package org.geoserver.security.oauth2.services;
 
-import java.io.UnsupportedEncodingException;
+import io.jsonwebtoken.Jwts;
+import org.geoserver.security.oauth2.GeoServerOAuthRemoteTokenServices;
+
 import java.util.Base64;
 import java.util.Map;
-import org.geoserver.security.oauth2.GeoServerOAuthRemoteTokenServices;
-import org.springframework.http.HttpHeaders;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
  * Remote Token Services for Google token details.
@@ -25,32 +25,33 @@ public class GoogleTokenServices extends GeoServerOAuthRemoteTokenServices {
 
     @Override
     protected Map<String, Object> checkToken(String accessToken) {
-        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
-        formData.add("token", accessToken);
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", getAuthorizationHeader(accessToken));
-        String accessTokenUrl = new StringBuilder(checkTokenEndpointUrl)
-                .append("?access_token=")
-                .append(accessToken)
-                .toString();
-        return postForMap(accessTokenUrl, formData, headers);
+        try {
+            LOGGER.debug("CRG check access token [{}]", accessToken);
+
+            return Jwts.parser()
+                       .setSigningKey(clientSecret.getBytes())
+                       .parseClaimsJws(accessToken)
+                       .getBody();
+        } catch (Exception e) {
+            LOGGER.error("Failed to parse access token. Reason => {}", e.getMessage(), e);
+
+            return Map.of();
+        }
     }
 
     @Override
     protected void transformNonStandardValuesToStandardValues(Map<String, Object> map) {
-        LOGGER.debug("Original map = " + map);
-        map.put("client_id", map.get("issued_to")); // Google sends 'client_id' as 'issued_to'
-        map.put("user_name", map.get("user_id")); // Google sends 'user_name' as 'user_id'
-        LOGGER.debug("Transformed = " + map);
+        LOGGER.debug("Original map = {}", map);
+
+        map.put("client_id", map.get("user_name"));
+
+        LOGGER.debug("Transformed = {}", map);
     }
 
     @Override
     protected String getAuthorizationHeader(String accessToken) {
         String creds = "%s:%s".formatted(clientId, clientSecret);
-        try {
-            return "Basic " + new String(Base64.getEncoder().encode(creds.getBytes("UTF-8")));
-        } catch (UnsupportedEncodingException e) {
-            throw new IllegalStateException("Could not convert String");
-        }
+
+        return "Basic " + new String(Base64.getEncoder().encode(creds.getBytes(UTF_8)));
     }
 }
